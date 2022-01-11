13c6c2cced42c4734cbf787d043e1a901e3df1b0
Daniel Filipe Schwingel <danielfilipeschwingel@gmail.com>
2022-01-10 23:19:31 -0300

    Created project and added electron

---
+++ index.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+
+   const win = new BrowserWindow({
+      width: 800,
+      height: 600,
+      webPreferences: {
+         nodeIntegration: true
+      }
+   });
+   win.loadURL('http://localhost:3000')
+}
+
+app.whenReady().then(createWindow)
